e032b44ede919314fe13f8635417c363432e710e
naskararpan03-gif <naskararpan03@gmail.com>
2026-03-29 22:00:13 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,8 @@
+function openPopup(src) {
+  document.getElementById("popup").style.display = "flex";
+  document.getElementById("popup-img").src = src;
+}
+
+function closePopup() {
+  document.getElementById("popup").style.display = "none";
+}
